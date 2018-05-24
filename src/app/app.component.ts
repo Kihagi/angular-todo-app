37210c34ef2f88266e6f33e52a012cd82d73656c
Mathenge <anthony@pezesha.com>
2018-05-24 11:53:27 +0300

    Seperate Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,30 @@
 import { Component } from '@angular/core';
 import {Todo} from './todo';
 import {TodoDataService} from './todo-data.service';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [TodoDataService]
+  providers: []
 })
 export class AppComponent {
 
-  newTodo: Todo = new Todo();
   constructor(private todoDataService: TodoDataService) {
   }
-  addTodo(todo) {
+
+  onAddTodo(todo: Todo) {
     this.todoDataService.addTodo(todo);
-    this.newTodo = new todo;
   }
-  toggleTodoComplete(todo) {
+
+  onToggleTodoComplete(todo: Todo) {
     this.todoDataService.toggleTodoComplete(todo);
   }
-  removeTodo(todo) {
+
+  onRemoveTodo(todo: Todo) {
     this.todoDataService.deleteTodoById(todo.id);
   }
+
   get todos() {
     return this.todoDataService.getAllTodos();
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+import { TodoListFooterComponent } from './todo-list-footer/todo-list-footer.component';
+import {TodoDataService} from './todo-data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListHeaderComponent,
+    TodoListComponent,
+    TodoListItemComponent,
+    TodoListFooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [TodoDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
